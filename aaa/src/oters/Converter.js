f1087eec67470a9d98751b81b9312d3dc53eeca0
kkh061101 <kkh061101@naver.com>
2022-11-01 21:02:04 +0900

    Update Converter.js

--- aaa/src/oters/Converter.js
+++ aaa/src/oters/Converter.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function Converter(){
+  const [minutes, setMinutes] = useState();
+  const minchange = (e)=>{
+    setMinutes(e.target.value);
+  }
   return(
     <div>
-      <label for='minutes'>min</label>
-      <input id='minutes' placeholder='minutes' type={'number'}/>
-      <label for='hours'>hours</label>
+      <h4>{minutes}</h4>
+      <label htmlFor='minutes'>min</label>
+      <input value={minutes} onChange={minchange} id='minutes' placeholder='minutes' type={'number'}/>
+      <label htmlFor='hours'>hours</label>
       <input id='hours' placeholder='hours' type={'number'}/>
     </div>
   );
