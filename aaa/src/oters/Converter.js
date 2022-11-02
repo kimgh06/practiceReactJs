be3fa9cf2a27a667c7c95a22568b07b910953297
kkh061101 <kkh061101@naver.com>
2022-11-02 19:51:02 +0900

    Update Converter.js

--- aaa/src/oters/Converter.js
+++ aaa/src/oters/Converter.js
@@ -1,27 +1,27 @@
 import React,{useState} from 'react';
 
 function Converter(){
-  const [minutes, setMinutes] = useState();
+  const [amount, setAmount] = useState();
   const [flipped, setFlipped] = useState(false);
   const minchange = (e)=>{
-    setMinutes(e.target.value);
+    setAmount(e.target.value);
   }
   return(
     <div>
       <div>
       <label htmlFor='minutes'>min : </label>
-      <input value={flipped ? 60*minutes: minutes} onChange={minchange} id='minutes' placeholder='minutes' type={'number'} disabled={flipped}/>
+      <input value={flipped ? 60*amount: amount} onChange={minchange} id='minutes' placeholder='minutes' type={'number'} disabled={flipped}/>
       </div>
       <div>
       <label htmlFor='hours'>hours : </label>
-      <input value={flipped ? minutes: minutes/60} onChange={minchange} id='hours' placeholder='hours' type={'number'} disabled={!flipped}/>
+      <input value={flipped ? amount: amount/60} onChange={minchange} id='hours' placeholder='hours' type={'number'} disabled={!flipped}/>
       </div>
       <button onMouseDown={()=>{
-        setMinutes(0);
+        setAmount(0);
       }}>Reset</button>
       <button onClick={()=>{
         setFlipped((current) =>!current);
-        setMinutes(flipped ? minutes * 60 : minutes / 60);
+        setAmount(flipped ? amount * 60 : amount / 60);
       }}>Flips</button>
     </div>
   );
