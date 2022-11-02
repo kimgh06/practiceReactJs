86209e6cdfad4efa2c4f401e07b0a3e1133ac034
kkh061101 <kkh061101@naver.com>
2022-11-02 19:49:38 +0900

    Update Converter.js

--- aaa/src/oters/Converter.js
+++ aaa/src/oters/Converter.js
@@ -10,17 +10,18 @@ function Converter(){
     <div>
       <div>
       <label htmlFor='minutes'>min : </label>
-      <input value={minutes} onChange={minchange} id='minutes' placeholder='minutes' type={'number'} disabled={flipped}/>
+      <input value={flipped ? 60*minutes: minutes} onChange={minchange} id='minutes' placeholder='minutes' type={'number'} disabled={flipped}/>
       </div>
       <div>
       <label htmlFor='hours'>hours : </label>
-      <input value={minutes/60} id='hours' placeholder='hours' type={'number'} disabled={!flipped}/>
+      <input value={flipped ? minutes: minutes/60} onChange={minchange} id='hours' placeholder='hours' type={'number'} disabled={!flipped}/>
       </div>
       <button onMouseDown={()=>{
         setMinutes(0);
       }}>Reset</button>
       <button onClick={()=>{
         setFlipped((current) =>!current);
+        setMinutes(flipped ? minutes * 60 : minutes / 60);
       }}>Flips</button>
     </div>
   );
