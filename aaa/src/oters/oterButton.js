82c3be61dcd93d8ecbd48ca25c063b260ea625fa
kkh061101 <kkh061101@naver.com>
2022-11-03 19:32:30 +0900

    Update oterButton.js

--- aaa/src/oters/oterButton.js
+++ aaa/src/oters/oterButton.js
@@ -2,18 +2,11 @@ import React from 'react';
 import './oters.scss';
 
 function oterButton({text, onClick, Name}){
+  const oterText ='';
     
   return(
     <>
       <button onClick={onClick} className={Name}>{text}</button>
-    </>
-  );
-}
-
-function oterText(){
-  return (
-    <>
-      
     </>
   );
 }
