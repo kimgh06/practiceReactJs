4bff210f447ce8ee61f4177d50350c440a84b359
kkh061101 <kkh061101@naver.com>
2022-11-07 19:47:03 +0900

    add proptypes

--- aaa/src/App.js
+++ aaa/src/App.js
@@ -1,12 +1,19 @@
 import React,{useState} from 'react';
+import PropTypes from "prop-types";
 import './App.scss';
 import Oters from './oters/Oters';
 import OterButton, {oterText} from './oters/oterButton';
 
+
+OterButton.propTypes = {
+  text:PropTypes.string,
+  fontSize: PropTypes.number,
+};
 function App() {
   return(<>
   <Oters/>
   <OterButton text="321" fontSize={20}/>
+  <OterButton text={31} fontSize={"asdf"}/>
   </>
   );
 }
