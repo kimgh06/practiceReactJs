ce446f71ea539bebc59e88ae8aeaf3ec7ed1312e
kkh061101 <kkh061101@naver.com>
2022-11-10 19:08:03 +0900

    Update oterButton.js

--- aaa/src/oters/oterButton.js
+++ aaa/src/oters/oterButton.js
@@ -5,7 +5,7 @@ OterButton.propTypes = {
   text: PropTypes.string.isRequired,
   fontSize: PropTypes.number.isRequired,
 };
-function OterButton({ text, fontSize = 14, width = 200 }) {
+function OterButton({ text, fontSize = 14, width = 200, backgroundColor = "skyblue" }) {
   const [a, setA] = useState(text);
   const [t, setT] = useState(false);
   return (
@@ -13,6 +13,7 @@ function OterButton({ text, fontSize = 14, width = 200 }) {
       <button className='Oterbutton' style={{
         fontSize: fontSize,
         width: width,
+        backgroundColor: 'skyblue',
       }}
         onClick={() => {
           if (!t) {
