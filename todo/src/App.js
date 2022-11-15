07b38e18fd58cd3e1445885bbf1e5136a20c8857
kkh061101 <kkh061101@naver.com>
2022-11-16 08:57:44 +0900

    new Components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Todo from './Components/Todo1';
 import Coins from './Components/Coins';
+import Movie from './Components/Movie';
 
 function App() {
   return (
     <>
       {/* <Todo /> */}
-      <Coins />
+      {/* <Coins /> */}
+      <Movie />
     </>
   );
 }
---
+++ todo/src/Components/Movie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Movie() {
+  return (
+    <>
+      hello
+    </>
+  );
+}
+
+export default Movie;
