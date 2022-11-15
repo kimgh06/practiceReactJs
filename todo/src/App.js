7f1e3e8cece5619b4a352a1a2cac7955f1f02401
kkh061101 <kkh061101@naver.com>
2022-11-15 22:23:45 +0900

    changed the title

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const onC = (e) => {
+    setTodo(e.target.value);
+  }
   return (
     <>
-      hello
+      <form>
+        <div>
+          <input onChange={onC} value={todo} type="text" placeholder='Type things to do' />
+        </div>
+      </form>
     </>
   );
 }
