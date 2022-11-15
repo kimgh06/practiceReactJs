b6081c9a628820ce0d2b1761db3e0b39a4839aee
kkh061101 <kkh061101@naver.com>
2022-11-15 23:09:18 +0900

    add Coins

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import Todo from './Components/Todo1';
+import Coins from './Components/Coins';
 
 function App() {
   return (
-    <Todo />
+    <>
+      {/* <Todo /> */}
+      <Coins />
+    </>
   );
 }
 
---
+++ todo/src/Components/Coins.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Coins() {
+  return (
+    <>
+      hello
+    </>
+  );
+}
+
+export default Coins;
