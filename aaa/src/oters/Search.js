4408e21a4e1121199efa5ce6a786249be2cedbf7
kkh061101 <kkh061101@naver.com>
2022-11-15 17:58:00 +0900

    동적 styled 추가

--- aaa/src/oters/Search.js
+++ aaa/src/oters/Search.js
@@ -5,15 +5,21 @@ function Search({ search }) {
     console.log("반갑습네다.");
   }, []);
   const [key, setKey] = useState("");
+  const [styled, setStyled] = useState("");
   return (
     <div
-      className='search'>
+      className='search'
+      style={{ styled }}
+    >
       <input
         type="text"
         value={key}
         placeholder={search}
         onChange={(e) => {
           setKey(e.target.value);
+          setStyled({
+            backgroundColor: "blue",
+          });
         }} />
     </div>
   );
