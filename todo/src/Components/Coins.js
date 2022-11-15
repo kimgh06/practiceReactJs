8f72009849d48b672bb020f15d4e2a304d6c8359
kkh061101 <kkh061101@naver.com>
2022-11-15 23:23:07 +0900

    Update Coins.js

--- todo/src/Components/Coins.js
+++ todo/src/Components/Coins.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Coins() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers").then(r => r.json()).then(j => console.log(j));
+  }, []);
   return (
     <>
-      hello
+      <h1>The Coins!</h1>
+      {loading ? <b>Loading...</b> : ""}
     </>
   );
 }
