7b66bd4e5aa50900aca6b1a291cae9ba075876ee
kkh061101 <kkh061101@naver.com>
2022-11-16 09:10:13 +0900

    Update Movie.js

--- todo/src/Components/Movie.js
+++ todo/src/Components/Movie.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Movie() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year").then(r => r.json()).then(j => console.log(j));
+  }, []);
   return (
     <>
-      hello
+      {loading ? <h1>Loading...</h1> : ""}
     </>
   );
 }
