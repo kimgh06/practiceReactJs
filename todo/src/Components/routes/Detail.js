5749fdb3e8668d7fd8265c66500399bc17db8ce0
kkh061101 <kkh061101@naver.com>
2022-11-16 23:08:25 +0900

    Update Detail.js

--- todo/src/Components/routes/Detail.js
+++ todo/src/Components/routes/Detail.js
@@ -1,11 +1,39 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from 'react-router-dom'
 
 function Detail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const a = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+    setMovie(json.data.movie);
+  }
+  console.log(movie);
+  useEffect(() => {
+    a();
+  }, []);
   return (
-    <Link to='/'>
-      <h3>Details</h3>
-    </Link>
+    <>
+      <div style={{ display: 'flex' }}>
+        <div>
+          <img src={movie.large_cover_image} />
+        </div>
+        <div>
+          <Link to='/'>
+            <button>Return to Home</button>
+          </Link>
+          <ul>
+            <span>
+              <h1>{movie.title_long}</h1>
+            </span>
+            <li>
+              Description : "{movie.description_intro}"
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
