6c104035d147191ac46376dd7dc9286c1c45c833
kkh061101 <kkh061101@naver.com>
2022-11-16 10:14:00 +0900

    deleted then() and add async & await

--- todo/src/Components/Movie.js
+++ todo/src/Components/Movie.js
@@ -2,12 +2,37 @@ import React, { useState, useEffect } from "react";
 
 function Movie() {
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
   useEffect(() => {
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year").then(r => r.json()).then(j => console.log(j));
+    getMovies();
   }, []);
+  console.log(movies);
   return (
     <>
-      {loading ? <h1>Loading...</h1> : ""}
+      <h1>Movies</h1>
+      <ul>
+        {loading ? <h1>Loading...</h1> : movies.map((i, n) =>
+          <li>
+            {i.title}
+            <ul>
+              <li>
+                language : ({i.language})
+              </li>
+              <li>
+                <a href={i.url} target='_blank'>Go to site</a>
+              </li>
+            </ul>
+          </li>
+        )}
+      </ul>
     </>
   );
 }
