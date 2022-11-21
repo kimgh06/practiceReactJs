a51b4395d9e6aa112104afe5fa219b2f3d3d8b63
kkh061101 <kkh061101@naver.com>
2022-11-21 20:55:11 +0900

    불필요한 장르 삭제

--- todo/src/Components/Movie.js
+++ todo/src/Components/Movie.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function Movie({ title, year, img, lang, summary, id, genres }) {
+function Movie({ title, year, img, lang, summary, id }) {
   return (
-    <div className="Movie">
+    <div className={`Movie ${id}`}>
       <Link to={`/movie/${id}`} title={`Go to see more "${title}"`}>
         <img src={img} />
       </Link>
@@ -28,7 +28,6 @@ Movie.propTypes = {
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf().isRequired,
 };
 
 export default Movie;
