4720c42fcfdfc5af2bdae5971d05347018436df6
kkh061101 <kkh061101@naver.com>
2022-11-30 15:55:11 +0900

    input 태그를 이용한 검색 기능 추가

--- pokemon/src/Components/Poke.js
+++ pokemon/src/Components/Poke.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 function Poke() {
   const [id, setId] = useState(25);
   const [poke, setPoke] = useState([]);
+  const [inputs, setInput] = useState(id);
   const fetching = async () => {
     const json = await (
       await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)).json();
@@ -12,10 +13,27 @@ function Poke() {
   useEffect(() => {
     fetching();
   }, []);
+  const Submit = (e) => {
+    e.preventDefault();
+    setId(inputs);
+    fetching();
+  }
   return (
     <div>
+      <form onSubmit={Submit}>
+        <input value={inputs} onChange={(e) => {
+          setInput(e.target.value);
+        }} />
+        <button>Submit</button>
+      </form>
       <ul>
         {poke.name}
+        <li>
+          {Math.ceil(poke.height * 30.48) / 100} m
+        </li>
+        <li>
+          {Math.ceil(poke.weight * 45.3592) / 100} kg
+        </li>
       </ul>
     </div>
   );
