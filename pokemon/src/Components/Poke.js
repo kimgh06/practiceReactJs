31276f8dc265f7cbb5aa8e2e9c344b72719620b9
kkh061101 <kkh061101@naver.com>
2022-12-01 20:12:45 +0900

    진화 후 표시 연산자 추가

--- pokemon/src/Components/Poke.js
+++ pokemon/src/Components/Poke.js
@@ -92,16 +92,17 @@ function Poke() {
               </li>
             }
             {
-              species.habitat.name !== 'rare' ? evolve.chain.species.name === poke.name && evolve.chain.evolves_to.length &&
+              evolve.chain.evolves_to.length && species.habitat.name !== 'rare' ? evolve.chain.species.name === poke.name &&
                 <li>evolves to : {evolve.chain.evolves_to[0].species.url.slice(42, -1)}
                   &nbsp;{evolve.chain.evolves_to[0].species.name}
                 </li> : ''
             }
             {
-              species.habitat.name !== 'rare' ? evolve.chain.evolves_to[0].species.name === poke.name && evolve.chain.evolves_to[0].evolves_to.length && evolve.chain.evolves_to.length &&
+              evolve.chain.evolves_to.length && species.habitat.name !== 'rare' ? (evolve.chain.evolves_to[0].evolves_to.length ?
+                evolve.chain.evolves_to[0].species.name === poke.name &&
                 < li > evolves to: {evolve.chain.evolves_to[0].evolves_to[0].species.url.slice(42, -1)}
                   &nbsp;{evolve.chain.evolves_to[0].evolves_to[0].species.name}
-                </li> : ''
+                </li> : '') : ''
             }
           </ul>
         </div>
